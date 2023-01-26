079a1cc9b118ef0abd3934c16767d5de054987e9
Documenter.jl <documenter@juliadocs.github.io>
2023-01-26 16:30:41 +0000

    build based on 2a384a6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GraduatedNonConvexity","category":"page"},{"location":"#GraduatedNonConvexity","page":"Home","title":"GraduatedNonConvexity","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GraduatedNonConvexity.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GraduatedNonConvexity]","category":"page"}]
+}
